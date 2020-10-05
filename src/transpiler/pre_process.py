97bf93347a173febc2e955063b2ff5b9ef740eb2
Jampi0n <27952849+Jampi0n@users.noreply.github.com>
2020-10-05 15:27:06 +0200

    better comment removal

--- src/transpiler/pre_process.py
+++ src/transpiler/pre_process.py
@@ -1,4 +1,55 @@
 import re
+from enum import Enum
+
+
+class ReadState(Enum):
+    NORMAL = 1
+    STRING = 2
+    SINGLE_COMMENT = 3
+    MULTI_COMMENT = 4
+
+
+def remove_comments(code):
+    state = ReadState.NORMAL
+    escape = False
+    result = ''
+    i = 0
+    while i < (len(code)):
+        c = code[i]
+        if state == ReadState.NORMAL:
+            if c == '"':
+                state = ReadState.STRING
+                escape = False
+            if i + 1 < len(code):
+                if c + code[i + 1] == '//':
+                    state = ReadState.SINGLE_COMMENT
+                    i += 2
+                    continue
+                if c + code[i + 1] == '/*':
+                    state = ReadState.MULTI_COMMENT
+                    i += 2
+                    continue
+            result += c
+        elif state == ReadState.STRING:
+            if escape:
+                escape = False
+            else:
+                if c == '"':
+                    state = ReadState.NORMAL
+                if c == '\\':
+                    escape = True
+            result += c
+        elif state == ReadState.SINGLE_COMMENT:
+            if c == '\n':
+                state = ReadState.NORMAL
+                result += c
+        elif state == ReadState.MULTI_COMMENT:
+            if i + 1 < len(code):
+                if c + code[i + 1] == '*/':
+                    state = ReadState.NORMAL
+                    i += 1
+        i += 1
+    return result
 
 
 class PreProcess:
@@ -14,8 +65,7 @@ class PreProcess:
         description = re.search(multi_line, self.string).group(0)
         unit = re.search('\\n\\s*// unit .*', self.string).group(0)
 
-        self.string = re.sub(multi_line, '', self.string)
-        self.string = re.sub('\\n\\s*//.*', '', self.string)
+        self.string = remove_comments(self.string)
 
         header = '{' + description[2:-2] + '}\n\n' + unit[5:] + ';\n\n'
         return header, self.string
